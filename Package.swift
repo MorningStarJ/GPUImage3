79a69083315aa4488c61f4ee161c192144a8482c
MorningStar <jngviho@icloud.com>
2020-10-10 18:17:25 +0800

    update Swift 5

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GPUImage",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9),
+        .macOS(.v10_13), .iOS(.v11),
     ],
     products: [
         .library(
